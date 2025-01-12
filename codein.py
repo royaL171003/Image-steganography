0afda1d66de0c0a05bd3c49de85f550168fbc2a4
royaL171003 <129684946+royaL171003@users.noreply.github.com>
2025-01-12 20:04:39 +0530

    codein.py

--- codein.py
+++ codein.py
@@ -1,54 +1,138 @@
-import argparse
-import os
-import sys
-from modules.paths_internal import models_path, script_path, data_path, extensions_dir, extensions_builtin_dir
-import modules.safe
+# Python program implementing Image Steganography
 
-# data_path = cmd_opts_pre.data
-sys.path.insert(0, script_path)
+# PIL module is used to extract
+# pixels of image and modify it
+from PIL import Image
 
-# search for directory of stable diffusion in following places
-sd_path = "/home/studio-lab-user/mydrive/sd/stable-diffusion"
-# possible_sd_paths = [os.path.join(script_path, '/mydrive/sd/stablediffusion'), '.', os.path.dirname(script_path)]
-# for possible_sd_path in possible_sd_paths:
-#     print(os.path.join(possible_sd_path, 'ldm/models/diffusion/ddpm.py'))
-#     if os.path.exists(os.path.join(possible_sd_path, 'ldm/models/diffusion/ddpm.py')):
-#         sd_path = os.path.abspath(possible_sd_path)
-#         break
+# Convert encoding data into 8-bit binary
+# form using ASCII value of characters
+def genData(data):
 
-# assert sd_path is not None, "Couldn't find Stable Diffusion in any of: " + str(possible_sd_paths)
+		# list of binary codes
+		# of given data
+		newd = []
 
-path_dirs = [
-    (sd_path, 'ldm', 'Stable Diffusion', []),
-    (os.path.join(sd_path, 'src/taming-transformers'), 'taming', 'Taming Transformers', []),
-    (os.path.join(sd_path, 'src/codeformer'), 'inference_codeformer.py', 'CodeFormer', []),
-    (os.path.join(sd_path, 'src/blip'), 'models/blip.py', 'BLIP', []),
-    (os.path.join(sd_path, 'src/k-diffusion'), 'k_diffusion/sampling.py', 'k_diffusion', ["atstart"]),
-]
+		for i in data:
+			newd.append(format(ord(i), '08b'))
+		return newd
 
-paths = {}
+# Pixels are modified according to the
+# 8-bit binary data and finally returned
+def modPix(pix, data):
 
-for d, must_exist, what, options in path_dirs:
-    must_exist_path = os.path.abspath(os.path.join(script_path, d, must_exist))
-    if not os.path.exists(must_exist_path):
-        print(f"Warning: {what} not found at path {must_exist_path}", file=sys.stderr)
-    else:
-        d = os.path.abspath(d)
-        if "atstart" in options:
-            sys.path.insert(0, d)
-        else:
-            sys.path.append(d)
-        paths[what] = d
-        
-class Prioritize:
-    def __init__(self, name):
-        self.name = name
-        self.path = None
+	datalist = genData(data)
+	lendata = len(datalist)
+	imdata = iter(pix)
 
-    def __enter__(self):
-        self.path = sys.path.copy()
-        sys.path = [paths[self.name]] + sys.path
+	for i in range(lendata):
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        sys.path = self.path
-        self.path = None 
+		# Extracting 3 pixels at a time
+		pix = [value for value in imdata.__next__()[:3] +
+								imdata.__next__()[:3] +
+								imdata.__next__()[:3]]
+
+		# Pixel value should be made
+		# odd for 1 and even for 0
+		for j in range(0, 8):
+			if (datalist[i][j] == '0' and pix[j]% 2 != 0):
+				pix[j] -= 1
+
+			elif (datalist[i][j] == '1' and pix[j] % 2 == 0):
+				if(pix[j] != 0):
+					pix[j] -= 1
+				else:
+					pix[j] += 1
+				# pix[j] -= 1
+
+		# Eighth pixel of every set tells
+		# whether to stop ot read further.
+		# 0 means keep reading; 1 means thec
+		# message is over.
+		if (i == lendata - 1):
+			if (pix[-1] % 2 == 0):
+				if(pix[-1] != 0):
+					pix[-1] -= 1
+				else:
+					pix[-1] += 1
+
+		else:
+			if (pix[-1] % 2 != 0):
+				pix[-1] -= 1
+
+		pix = tuple(pix)
+		yield pix[0:3]
+		yield pix[3:6]
+		yield pix[6:9]
+
+def encode_enc(newimg, data):
+	w = newimg.size[0]
+	(x, y) = (0, 0)
+
+	for pixel in modPix(newimg.getdata(), data):
+
+		# Putting modified pixels in the new image
+		newimg.putpixel((x, y), pixel)
+		if (x == w - 1):
+			x = 0
+			y += 1
+		else:
+			x += 1
+
+# Encode data into image
+def encode():
+	img = input("Enter image name(with extension) : ")
+	image = Image.open(img, 'r')
+
+	data = input("Enter data to be encoded : ")
+	if (len(data) == 0):
+		raise ValueError('Data is empty')
+
+	newimg = image.copy()
+	encode_enc(newimg, data)
+
+	new_img_name = input("Enter the name of new image(with extension) : ")
+	newimg.save(new_img_name, str(new_img_name.split(".")[1].upper()))
+
+# Decode the data in the image
+def decode():
+	img = input("Enter image name(with extension) : ")
+	image = Image.open(img, 'r')
+
+	data = ''
+	imgdata = iter(image.getdata())
+
+	while (True):
+		pixels = [value for value in imgdata.__next__()[:3] +
+								imgdata.__next__()[:3] +
+								imgdata.__next__()[:3]]
+
+		# string of binary data
+		binstr = ''
+
+		for i in pixels[:8]:
+			if (i % 2 == 0):
+				binstr += '0'
+			else:
+				binstr += '1'
+
+		data += chr(int(binstr, 2))
+		if (pixels[-1] % 2 != 0):
+			return data
+
+# Main Function
+def main():
+	a = int(input(":: Welcome to Steganography ::\n"
+						"1. Encode\n2. Decode\n"))
+	if (a == 1):
+		encode()
+
+	elif (a == 2):
+		print("Decoded Word : " + decode())
+	else:
+		raise Exception("Enter correct input")
+
+# Driver Code
+if __name__ == '__main__' :
+
+	# Calling main function
+	main()
